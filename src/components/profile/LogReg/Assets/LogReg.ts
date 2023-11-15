942fb5e4906cd40dc5b58ad07488cbf402c48175
gang_dramma <your_email@kottaholakottahola@gmail.com>
2023-11-15 23:02:18 +0500

    my codes

---
+++ src/components/profile/LogReg/Assets/LogReg.ts
@@ -0,0 +1,27 @@
+import React from "react";
+// export async function submit() {
+//     var user = {
+//       email: `${c.inpRegEmail.value}`,
+//       name: `${c.inpRegUserName.value}`,
+//       password: `${c.inpRegPassword.value}`
+//     };
+//     const newUser = await s.Auth.Register(user);
+//     console.log(`[New User] =`, newUser);
+//     c.register.style.display = 'none';
+//     c.login.style.display = 'block';
+//   }
+//   export async function logIn() {
+//     const token = await s.Auth.Login({
+//       email: `${c.inpLogEmail.value}`,
+//       password: `${c.inpLogPassword.value}`
+//     });
+//     c.login.style.display = 'none';
+//     c.navbar.style.display = 'block';
+//     c.main.style.display = 'block';
+//     c.registerBtn.innerText = 'Logout';
+//     c.loginBtn.innerText = `${c.inpRegUserName.value}`;
+//   }
+  
+//   c.submit.addEventListener('click', submit);
+//   c.singIn.addEventListener('click', logIn);
+  
